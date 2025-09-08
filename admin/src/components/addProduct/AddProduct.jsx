67643312d092d696e8a18648b252e8f8fc52ce8c
Nirmal Advani <nirmaladvani@live.com>
2025-09-08 09:11:25 +0500

    updated admin

--- admin/src/components/addProduct/AddProduct.jsx
+++ admin/src/components/addProduct/AddProduct.jsx
@@ -28,7 +28,7 @@ const AddProduct = () => {
     let formData = new FormData()
     formData.append('product', image)
 
-    await fetch('http://localhost:4000/upload', {
+    await fetch('http://e-commerce-production-fa31.up.railway.app/upload', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -42,14 +42,17 @@ const AddProduct = () => {
     if (responseData.success) {
       product.image = responseData.image_url
       console.log(product)
-      await fetch('http://localhost:4000/addproduct', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(product),
-      })
+      await fetch(
+        'http://e-commerce-production-fa31.up.railway.app/addproduct',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(product),
+        }
+      )
         .then((resp) => resp.json())
         .then((data) => {
           data.success ? alert('Product Added') : alert('Failed')
--- admin/src/components/listProduct/ListProduct.jsx
+++ admin/src/components/listProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([])
 
   const fetchInfo = async () => {
-    await fetch('http://localhost:4000/allproducts')
+    await fetch('http://e-commerce-production-fa31.up.railway.app/allproducts')
       .then((resp) => resp.json())
       .then((data) => {
         setAllProducts(data)
@@ -18,14 +18,17 @@ const ListProduct = () => {
   }, [])
 
   const remove_product = async (id) => {
-    await fetch('http://localhost:4000/removeproduct', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ id: id }),
-    })
+    await fetch(
+      'http://e-commerce-production-fa31.up.railway.app/removeproduct',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id: id }),
+      }
+    )
     await fetchInfo()
   }
 
